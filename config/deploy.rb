7457d75ce9b244bb34bc786073a8885ec3dec413
Alex Smith <alex@roadreadywheels.com>
2018-02-11 12:57:28 -0700

    fixed gem file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,29 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "dividend_tracker"
-set :repo_url, "alex@roadreadywheels.com:roadreadywheels/dividend_tracker.git"
-set :deploy_user, "deploy"
+set :application, 'dividend_tracker'
+set :repo_url, 'git@github.com:roadreadywheels/dividend_tracker.git' # Edit this to match your repository
+set :branch, :master
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}" 
+set :pty, true
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 5
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -11,7 +31,7 @@ set :deploy_user, "deploy"
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 
-set :deploy_to, "/home/#{fetch :deploy_user}/#{fetch :application}" 
+# set :deploy_to, "/home/#{fetch :deploy_user}/#{fetch :application}" 
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
