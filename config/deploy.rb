082fc0cc5b91f7000765d60fbdb170c189d4724c
Alex Smith <alex@roadreadywheels.com>
2018-02-11 18:59:12 -0700

    changed deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.10.1"
 set :application, 'dividend_tracker'
 set :repo_url, 'git@github.com:roadreadywheels/dividend_tracker.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}" 
+set :deploy_to, "/home/delpoy/dividend_tracker" 
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
@@ -24,6 +24,8 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
+
+SSHKit.config.command_map[:rake]  = "/.rvm/bin/#{fetch(:application)}_bundle exec rake"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
