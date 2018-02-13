f3c17d9af5049f08d25c448ffe66b751a9823655
Alex Smith <alex@roadreadywheels.com>
2018-02-12 19:09:30 -0700

    changed production server address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "172.31.28.254", user: "deploy", roles: %w[web app db]
+server "34.217.106.56", user: "deploy", roles: %w[web app db]
 
 # role-based syntax
 # ==================
