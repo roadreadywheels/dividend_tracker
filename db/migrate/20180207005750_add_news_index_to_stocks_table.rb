38981507cf87b0d85021d25fa04c877852a8d0de
Alex Smith <alex@roadreadywheels.com>
2018-02-13 20:36:52 -0700

    changed migration add news index to stocks

--- db/migrate/20180207005750_add_news_index_to_stocks_table.rb
+++ db/migrate/20180207005750_add_news_index_to_stocks_table.rb
@@ -2,7 +2,6 @@ class AddNewsIndexToStocksTable < ActiveRecord::Migration[5.1]
   def change
     add_column 'news', 'stock_id', :integer
     add_index('news', 'stock_id')
-    add_column "stocks", "yearly_income", :float, default: 0.0
   end
 
 end
