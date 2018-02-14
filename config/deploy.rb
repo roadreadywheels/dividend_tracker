a9bf656a706971c60111872787f8f58cce719052
Alex Smith <alex@roadreadywheels.com>
2018-02-14 12:22:31 -0700

    changed puma bind x 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ append :linked_files, "config/secrets.yml.key"
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix:///home/deploy/dividend_tracker/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_bind, "unix:///home/deploy/dividend_tracker/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
