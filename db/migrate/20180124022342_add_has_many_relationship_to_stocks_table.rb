e60b27780d903bfff0d2158c09228c5d70c05666
Alex Smith <alex@roadreadywheels.com>
2018-02-13 20:35:30 -0700

    changed migration add declared amount

--- db/migrate/20180124022342_add_has_many_relationship_to_stocks_table.rb
+++ db/migrate/20180124022342_add_has_many_relationship_to_stocks_table.rb
@@ -1,8 +1,3 @@
 class AddHasManyRelationshipToStocksTable < ActiveRecord::Migration[5.1]
-  
-  def change
-    add_column 'stocks', 'account_id', :integer
-    add_index('stocks', 'account_id')
-  end
 
 end
