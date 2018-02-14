891cb965debe6150b64ff34932829a69a9a5fb35
Alex Smith <alex@roadreadywheels.com>
2018-02-13 20:31:59 -0700

    changed migration add company financial dets to stock

--- db/migrate/20180123022912_add_company_financial_dets_to_stock.rb
+++ db/migrate/20180123022912_add_company_financial_dets_to_stock.rb
@@ -1,6 +1,6 @@
 class AddCompanyFinancialDetsToStock < ActiveRecord::Migration[5.1]
-  def up
-    add_column "stocks", "stock_price", :float
+  def up 
+    # add_column "stocks", "stock_price", :float
     add_column "stocks", "eps", :float
     add_column "stocks", "pe", :float
     add_column "stocks", "market_cap", :float
@@ -12,6 +12,6 @@ class AddCompanyFinancialDetsToStock < ActiveRecord::Migration[5.1]
     remove_column "stocks", "market_cap"
     remove_column "stocks", "pe"
     remove_column "stocks", "eps"
-    remove_column "stocks", "stock_price"
+   # remove_column "stocks", "stock_price"
   end
 end
