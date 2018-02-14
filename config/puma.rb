cee34681fa098d7b758e4114db465982d92e4596
Alex Smith <alex@roadreadywheels.com>
2018-02-13 21:04:05 -0700

    updated puma.rb file

--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,11 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+application_path = '/home/deploy/dividend_tracker/shared'
+
+bind "unix://#{application_path}/sockets/puma.socket"
+
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
