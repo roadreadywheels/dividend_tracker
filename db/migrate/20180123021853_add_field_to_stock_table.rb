5478dbe336891c879e8293c53981e9ecc8247d7d
Alex Smith <alex@roadreadywheels.com>
2018-02-13 20:28:23 -0700

    changed migration add field to stock table

--- db/migrate/20180123021853_add_field_to_stock_table.rb
+++ db/migrate/20180123021853_add_field_to_stock_table.rb
@@ -1,9 +1,3 @@
 class AddFieldToStockTable < ActiveRecord::Migration[5.1]
-  def up
-    add_column "stocks", "company_details", :string
-  end
-
-  def down
-    remove_column "admin_users", "company_details"
-  end
+  
 end
