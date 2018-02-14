241f80003897d3ee6510c139a497846067ec42fb
Alex Smith <alex@roadreadywheels.com>
2018-02-13 20:30:01 -0700

    changed migration add company name to stock table

--- db/migrate/20180123022235_add_company_name_to_stock_table.rb
+++ db/migrate/20180123022235_add_company_name_to_stock_table.rb
@@ -1,9 +1,3 @@
 class AddCompanyNameToStockTable < ActiveRecord::Migration[5.1]
-  def up
-    add_column "stocks", "company_name", :string
-  end
 
-  def down
-    remove_column "admin_users", "company_name"
-  end
 end
