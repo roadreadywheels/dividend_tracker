6ec2979c58da0efb022a6ba9cf9f492a8794f580
Alex Smith <alex@roadreadywheels.com>
2018-02-17 20:45:19 -0700

    sk attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "track_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "BABS_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -89,3 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+# server "34.217.106.56", user: "deploy", roles: %w[web app db], :primary => true
